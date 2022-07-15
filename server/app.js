6ff33a5893986ee669d292fcc7b0c3e5b2f65098
“danielwarga” <danielwarga@gmail.com>
2022-07-15 12:48:16 +0100

    working server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,66 @@
 const express = require ("express");
+const cors = require ("cors");
+
+const data = require ("./data");
 
 // Create an Express app
 const app = express();
+
+// Tell the app to listen to JSON bodies on POST requests
+app.use(express.json());
+
+// Add 'headers' to each response, saying that we're okay with sharing
+app.use(cors());
 
 // Set up a route
 app.get("/", (req, res) => {
     res.send("Welcome to the Ice Cream API!");
 })
 
-// ACtually start the server ------move this to app.js to test them separately
-// app.listen(3000, () => {
-//     console.log("Started lidtening on port 3000")
-// })
+app.get("/flavours", (req, res) => {
+
+    let flavours = data;
+
+    if (req.query.vegan) {
+        flavours = flavours.filter(f => f["vegan"])
+    }
+
+    res.json({
+        flavours: flavours.map(f => f["flavour"])
+    })
+})
+
+app.get("/flavours/:id", (req, res) => {
+
+    // Extract athe parameter from the 
+    const id = req.params.id;
+
+    // Filter the data
+    const filteredData = data.filter(f => f["id"] == id);
+
+    if(filteredData.length == 1) {
+        res.json({
+            flavour: filteredData[0]
+        })
+    }else {
+        res.status(404).json({
+            error:"no such ice cream"
+        })
+    }
+})
+
+
+app.post("/flavours", (req, res) => {
+    console.log(req.body);
+
+    const newFlavour = req.body;
+    const newId = data.length +1;
+    data.push(newFlavour)
+
+    res.status(201).json({
+        success: true,
+        flavour: newFlavour
+    })
+})
 
 module.exports = app;
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -17,4 +17,28 @@ describe("API", () => {
     it("Response to a GET request at / with a 200 status", (done) => {
         request(api).get("/").expect(200, done);
     })
+
+    it("Response to a GET request at /flavours with a 200 status", (done) => {
+        request(api).get("/flavours").expect(200, done);
+    })
+
+    it("Response to a GET request at /flavours with a JSON object", (done) => {
+        request(api).get("/flavours").expect('Content-Type', /json/, done);
+    })
+
+    // it("Response to a GET request at /flavours with a JSON object that has a 'flavours' key", (done) => {
+    //     request(api).get("/flavours").end((err, res) => {
+    //         try {
+    //             expect.assertions(2);
+    //             const data = res.body;
+    //             expect("flavours" in data);
+    //             expect(data["flavours"] instanceof Array);
+    //             done();
+    //         }catch (err){
+    //             done(err);
+    //         }
+    //     })
+    // })
 })
+
+{ flavours: ["strawberry"]}
